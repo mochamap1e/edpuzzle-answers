afc9fa336d14a395a374e9b49d72f3e002c2eff0
alex <alex.lopez81510@gmail.com>
2025-10-02 09:08:43 -0400

    asd

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript: fetch("https://cdn.jsdelivr.net/gh/ading2210/edpuzzle-answers@latest/script.js").then(r => r.text()).then(r => eval(r))
+javascript: fetch("https://cdn.jsdelivr.net/gh/mochamap1e/edpuzzle-answers@latest/script.js").then(r => r.text()).then(r => eval(r))
